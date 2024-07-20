aaad3da11095d4ef5f6661beeec1648fefee5e67
Dulmika <semaldulmika@gmail.com>
2024-07-20 18:27:13 +0530

    Create BookTransactionHistory entity

---
+++ src/main/java/com/dulmika/book/history/BookTransactionHistory.java
@@ -0,0 +1,23 @@
+package com.dulmika.book.history;
+
+import com.dulmika.book.common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+    //User relationships
+    //Book relationships
+
+    private boolean returned;
+    private boolean returnApproved;
+}
